d60174a393aff5e065aa03431c33c26f7f53efd4
monica88lima <flima.monica@gmail.com>
2024-06-21 22:58:38 -0300

    inicio tela jogo

--- palavra-secreta/src/components/JogandoPalavraSecreta.jsx
+++ palavra-secreta/src/components/JogandoPalavraSecreta.jsx
@@ -1,14 +1,34 @@
-import "./JogandoPalavraSecreta.css"
+import "./JogandoPalavraSecreta.css";
 
-const JogandoPalavraSecreta = ({verificaLetra}) => {
+const JogandoPalavraSecreta = ({ verificaLetra }) => {
   return (
-    <div>
-      <p>JogandoPalavraSecreta</p>
-
-    <button onClick={verificaLetra}>Finalizar</button>
+    <div className="game">
+      <p className="pontuacao">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Advinhe a Palavra </h1>
+      <h3 className="dica">
+        Dica sobre a palavra:
+        <span> dica....</span>
+      </h3>
+      <div className="containerPalavra">
+        <span className="letra">A</span>
+        <span className="quadrovazio"></span>
+      </div>
+      <div className="containerLetra">
+        <p>Tente Advinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letra" maxLength={1} required />
+          <button type="submit">Jogar</button>
+        </form>
+      </div>
+      <div className="letrasUsadas">
+        <p>Letras já utilizadas!</p>
+        <span>A,</span>
+        <span>G, </span>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default JogandoPalavraSecreta
+export default JogandoPalavraSecreta;
